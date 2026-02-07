e8ddb534159b180d651e396264e5bf3a3b15f973
Pratik-Bhsakar-Sumbe <mr.sumbepratik@gmail.com>
2026-02-08 00:52:56 +0530

    Add files via upload
    Age Checker

---
+++ Day_4.js
@@ -0,0 +1,16 @@
+let age = prompt("what is your age?")
+age = Number.parseInt(age)
+switch(age){
+  case 12:
+    console.log("your age is 12")
+    break
+  case 13:  
+    console.log("your age is 13")
+    break
+  case 14:
+    console.log("your age is 14")
+    break
+  default: 
+    console.log("your age is not special")
+
+}

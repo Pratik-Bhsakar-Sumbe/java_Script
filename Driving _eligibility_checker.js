9740d281a78a4822b2ba7e4af012b9db98cf06c2
Nobody <mr.sumbepratik@gmail.com>
2026-02-08 10:52:55 +0530

    Add files via upload
    driving eligibility check

---
+++ Driving _eligibility_checker.js
@@ -0,0 +1,9 @@
+alert("Welcome to the Driving Motor vehicle")
+let age = prompt("enter the age here....")
+age = Number.parseInt(age)
+if (age<17){
+  console.log("you cannot drive a vehicle your a minor.")
+}
+else{
+  console.log("you can drive the car")
+}
